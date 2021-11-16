8d64079520768f95ad9c500a44352f9f97541105
Ravindra Shinde <neelravi@gmail.com>
2021-11-16 13:05:09 +0100

    python pytests added

---
+++ tests01.py
@@ -0,0 +1,29 @@
+import pytest
+import cclib
+import qc2champ
+import numpy as np
+import sys
+import os
+
+for filename in ["sample_h2_rasscf.out"]:
+    basename = (filename.split(".", 1)[0])
+    data = cclib.io.ccread(filename)
+    # qc2champ.io.ccwrite(data, outputtype="xyz", outputdest="output.xyz")
+    # qc2champ.io.write_champ_v2_geometry(data, outputdest="CN3_" + basename)
+
+def test_cclib():
+	assert data.natom == 2
+	assert data.nbasis == 4
+	np.testing.assert_almost_equal(data.scfenergies[0], -30.552003252900377)
+
+
+def test_qc2champ():
+	assert data.ci['Number of CSFs'] == '2'
+	assert data.ci['Number of determinants'] == '2'
+	assert data.ci['Number of root(s) required'] == '1'
+	assert data.wfn_info['nelec_active_shell'] == '2'
+	assert data.metadata['package_version'] == '8.4'
+
+
+test_cclib()
+test_qc2champ()
